b6e0493dd898d846783d6febed84f89038411ae4
JohnMonteiro <jcmonteiro88@gmail.com>
2023-07-17 02:13:12 +0100

    Project Creation

---
+++ Article.cs
@@ -0,0 +1,11 @@
+﻿namespace TekkenPortugal.WebApi
+{
+    public class Article
+    {
+        public int Id { get; set; }
+     
+        public string Hero { get; set; } = string.Empty;
+
+        public string Thumbnail { get; set; } = string.Empty;
+    }
+}
---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TekkenPortugal.WebApi.Data;
+
+namespace TekkenPortugal.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ArticleController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Article>>> GetArticle()
+        {
+            return Ok(await _context.Article.ToListAsync());
+        }
+    }
+}
